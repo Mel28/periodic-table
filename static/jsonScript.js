ad564f5bd9d18c5c0f1fa421368130f7c6915e28
Melanie Milsted <melanie.milsted@gmail.com>
2019-03-18 17:34:11 +0000

    added comments to jsonScript.js

--- static/jsonScript.js
+++ static/jsonScript.js
@@ -11,7 +11,10 @@ function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
     xobj.open('GET', 'static/data/elementData.json', true); // Makes the call to the file path
+    
+    //onreadystatechange contains the event handler to be called when the readystatechange event is fired
     xobj.onreadystatechange = function () {
+        //readystate must be: complete and response received (4) or status 200 in console
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
             callback(xobj.responseText);
